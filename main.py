914e213cbec4eb69edf79bb825bcb2d981e6b6bf
codefatherru <vasiliy.m.leonov@gmail.com>
2023-02-22 00:04:13 +0300

    быстрое решение для хитрых клиентов

--- main.py
+++ main.py
@@ -62,20 +62,33 @@ if __name__ == '__main__':
         # проверим, не Ответ ли это от Админов
         if ((message["from"]["id"] in admins) and ("reply_to_message" in message)):
             # @todo возможно надо добавить проверку, что переслано сообщение от 6206108722 т.е. от самого бота
-            #формируем текст отчета об Ответе
-            rep = "исходное сообщение:" + "\nот " + str(message["reply_to_message"]["forward_from"]["id"]) + "\n" + \
-                  message["reply_to_message"]["forward_from"]["first_name"] + " "
-            # фамилия может быть не заполнена
-            if ("last_name" in message["reply_to_message"]["forward_from"]):
-                rep += message["reply_to_message"]["forward_from"]["last_name"]
-            rep +=  "\n" + message["reply_to_message"]["text"]
-            print(rep)
-            #соединяемся с персональным чатом автора исходного сообщения(Клиент, отправивший обращение с кодом)
-            reply = bot.channel(message["reply_to_message"]["forward_from"]["id"])
-            #отправляем Клиенту текст из сообщения-Ответа Админа
-            await reply.send_text(message["text"])
-            #отправляем отчет Админу
-            return await chat.reply("передано\n"+rep)
+            if(("reply_to_message" in message) and ("*INBOX from*" in message["reply_to_message"]["text"])):
+                #поймали ответ на техническое сообщение
+                data = message["reply_to_message"]["text"].split("*")
+                print(data)
+                rep = "исходное сообщение:" + "\nот " + data[2] + "\n" +data[3]
+                # соединяемся с персональным чатом автора исходного сообщения(Клиент, отправивший обращение с кодом)
+                reply = bot.channel(data[2])
+                await reply.send_text(message["text"])
+                return await chat.reply("передано\n" + rep)
+            elif ("forward_from" in message["reply_to_message"]):#это ответ на обычное пересланное Обращение
+                #формируем текст отчета об Ответе
+                rep = "исходное сообщение:" + "\nот " + str(message["reply_to_message"]["forward_from"]["id"]) + "\n" + \
+                      message["reply_to_message"]["forward_from"]["first_name"] + " "
+                # фамилия может быть не заполнена
+                if ("last_name" in message["reply_to_message"]["forward_from"]):
+                    rep += message["reply_to_message"]["forward_from"]["last_name"]
+                rep +=  "\n" + message["reply_to_message"]["text"]
+                print(rep)
+                #соединяемся с персональным чатом автора исходного сообщения(Клиент, отправивший обращение с кодом)
+                reply = bot.channel(message["reply_to_message"]["forward_from"]["id"])
+                #отправляем Клиенту текст из сообщения-Ответа Админа
+                await reply.send_text(message["text"])
+                #отправляем отчет Админу
+                return await chat.reply("передано\n"+rep)
+            elif ("forward_sender_name" in message["reply_to_message"]):#это ответ на непересылаемое сообщение
+                return await chat.reply("Клиент не разрешил пересылать свои Обращения, используйте техническое сообщение")
+
 
         #если текст входящего сообщение подходит под формат Кода (3 числа)
         if (re.fullmatch(r"(\d{3})", message['text'])):
@@ -83,7 +96,10 @@ if __name__ == '__main__':
             #await channel.forward_message(chat.id, message['message_id'])
             #пересылаем Обращение Админам
             for ch in channels:
+                #Пересылаем исхожное Обращение
                 await ch.forward_message(chat.id, message['message_id'])
+                #отправляем техническое сообщение
+                await ch.send_text("*INBOX from*" + str(message["from"]['id']) +"*" + message["text"], markup="MarkdownV2")
             return await chat.reply("Информацию принял, передаю. Ждите ответа")
         #отвечаем на все остальные неопознанные сообщения
         return chat.reply("Введите только цифры кода")
